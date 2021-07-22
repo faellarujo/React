d4238f2dc76adec755bf81dbfff8851f738d675b
Raphael <raphaelaraujo.ti@gmail.com>
2021-07-22 16:28:09 -0300

    Novos componentes

---
+++ src/componentes/Card/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Style.css'
+ 
+
+
+export default function Card() {
+    return(
+        <div className="card-container">
+            <div>
+                <p>Componentes:</p>
+                <p>Facilita em manter partes menores funcionando corretamente</p>
+                <p>Você pode reutilizá-los, ou seja, menos código para se escrever</p>
+            </div>
+        </div>
+        
+    )
+}
